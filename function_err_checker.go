8a088cb48245aae2777a885271704886f71809e8
Kanata Miyahana <afiri.bupa@gmail.com>
2022-06-20 21:24:25 +0900

    update

--- function_err_checker.go
+++ function_err_checker.go
@@ -12,7 +12,7 @@ const doc = "function_err_checker is ..."
 
 // Analyzer is ...
 var Analyzer = &analysis.Analyzer{
-	Name: "function_err_checker",
+	Name: "checker",
 	Doc:  doc,
 	Run:  run,
 	Requires: []*analysis.Analyzer{
@@ -28,12 +28,31 @@ func run(pass *analysis.Pass) (interface{}, error) {
 	}
 
 	inspect.Preorder(nodeFilter, func(n ast.Node) {
-		switch n := n.(type) {
-		case *ast.Ident:
-			if n.Name == "gopher" {
-				pass.Reportf(n.Pos(), "identifier is gopher")
+		if ifStmt, ok := n.(*ast.IfStmt); ok {
+			if assignStmt, ok := ifStmt.Init.(*ast.AssignStmt); ok {
+				if callExpr, ok := assignStmt.Rhs[0].(*ast.CallExpr); ok {
+					if ident, ok := callExpr.Fun.(*ast.Ident); ok {
+						if ident.Name == "Translate" {
+							if ifSt, ok := ifStmt.Body.List[0].(*ast.IfStmt); ok {
+								if ce, ok := ifSt.Cond.(*ast.CallExpr); ok {
+									if ide, ok := ce.Fun.(*ast.Ident); ok {
+										if ide.Name == "IsTypeError" {
+											pass.Reportf(ide.NamePos, "checked!")
+										}
+									}
+								}
+							}
+						}
+					}
+				}
 			}
 		}
+		// switch n := n.(type) {
+		// case *ast.Ident:
+		// 	if n.Name == "gopher" {
+		// 		pass.Reportf(n.Pos(), "identifier is gopher")
+		// 	}
+		// }
 	})
 
 	return nil, nil
